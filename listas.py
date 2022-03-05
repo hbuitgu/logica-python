4d6ef46ef30564e5fa96da8ae6f71ff899d8e3b5
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-03-04 20:57:53 -0500

    se crea un ejemplo de listas o arreglso en PY

--- listas.py
+++ listas.py
@@ -4,5 +4,5 @@ from unicodedata import numeric
 numeros=[1,2,3,4,5]
 print(numeros)
 #para recorrer arreglo es idear hacerlo con un for, es algo que crese o decrece 
-for i in range(1,30,5):
+for i in range(1,30,5):#en este caso reccore el rango desde 1 hasta 30 y muestra los numero de 5 posiciones
     print (i)
