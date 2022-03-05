432c0a2941f15534d5328f92d7f67329f590ac1b
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-03-04 20:56:06 -0500

    se crea ejemplo de lista

---
+++ listas.py
@@ -0,0 +1,8 @@
+from unicodedata import numeric
+
+
+numeros=[1,2,3,4,5]
+print(numeros)
+#para recorrer arreglo es idear hacerlo con un for, es algo que crese o decrece 
+for i in range(1,30,5):
+    print (i)
