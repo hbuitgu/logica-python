6e436cb06c5591d7de2ba4a1be67640bbd8682c5
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-03-04 19:02:57 -0500

    funcion if, para seleccion estación del año

---
+++ ejercicio_2.py
@@ -0,0 +1,15 @@
+##programar un componente que le pida el mes del año y pueda recibir 
+mes=input(("ingrese el mes del año: "))
+
+#caminos para clasificar el mes 
+
+if (mes=="diciembre"or mes =="enero"or mes=="febrero" or mes =="marzo"):
+    print("Estasn en Invierno")
+elif(mes=="junio"or mes=="julio"or mes=="agosto"):
+    print("estas en verano")
+elif(mes=="mayo"or mes=="abril"):
+    print("estas en Primavera")
+elif(mes=="septiembe"or mes=="octubre"or mes=="noviembre"):
+    print("estas en otoño")
+else:
+    print("el mes digitado no existe")
